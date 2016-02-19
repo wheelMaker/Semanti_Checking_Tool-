2472a89dcd535503c69faf42fbccd05fab2da24c
wywang <wyatt.wang@calix.com>
2016-02-19 09:30:43 +0800

    update on file_io and add ut cases

--- src/file_IO.py
+++ src/file_IO.py
@@ -6,23 +6,35 @@ _file_io_logger = logger.Logger(log_level='ERROR', log_module='FILEIO')
 
 
 class FileIOException(Exception):
+    _error_message = ''
     def __init__(self, msg=""):
-        _file_io_logger.log_exception(msg, module_name='FILEIO')
+        self._error_message = msg
+        print self._error_message
+        #_file_io_logger.log_exception(msg, module_name='FILEIO')
 
 
 class PyFileIO(object):
     _file_name = ''
     _file = ''
+    _mode = 'r'
 
     def __init__(self, file_name=''):
         if '' == file_name:
             raise FileIOException('File name is null!!!')
-        _file_name = file_name
+        self._file_name = file_name
 
-    def open_file(self, file_name='', mode='r'):
-        self._file = open(self._file_name, mode)
-        print 'wpf'
-        print self.file
+    def get_file_name(self):
+        return self._file_name
+
+    def set_file_name(self, file_name):
+        if '' == file_name:
+            raise FileIOException('File name is null!!!')
+        else:
+            self._file_name = file_name
+
+    def open_file(self, mode='r'):
+        self._mode = mode
+        self._file = open(self._file_name, self._mode)
 
     def close_file(self):
         self._file.close()
--- src/ut_fileIO.py
+++ src/ut_fileIO.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python
 
 import os
+import sys
+import re
 import file_IO as fIo
 
 current_path = os.getcwd()
 file_name = current_path + '/unit_test/ut_io_file'
 
+# case 1 for file open and close
+
 file_instance = fIo.PyFileIO(file_name)
 
-#  case for file open
+file_instance.open_file('w')
+file_instance.close_file()
+if os.path.isfile(file_instance.get_file_name()):
+    pass
+else:
+    raise fIo.FileIOException("Case 1 failed! File does not exist, open file function down")
+os.remove(file_instance.get_file_name())
+
+# case 2 for get file name
+
+try:
+    file_instance.set_file_name('a')
+    file_instance.open_file('w')
+    file_instance.close_file()
+except fIo.FileIOException:
+    print "Case 2 failed, null for the file name"
+
+if os.path.isfile(file_instance.get_file_name()):
+    raise fIo.FileIOException("Case 2 failed! File does not exist, open file function down")
+else:
+    pass
+
+
+
 
