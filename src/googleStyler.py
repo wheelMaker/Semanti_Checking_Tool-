bd1551401ad0fbb8b6c6bc3bba5e65c063bb3919
shenxiusi <shenxiusi@126.com>
2016-04-08 22:26:04 +0800

    add content and code property to class googlestyler and start to code for styler 2 exception format

--- src/googleStyler.py
+++ src/googleStyler.py
@@ -2,7 +2,6 @@
 Each styler in this class means a checking rule for GCS.
 Styler function must start with string "styler_" and end with its functionality
 GCSChecker.check() will invoke this class' stylers one by one for every single source file.
-Decorator should be added for all stylers!
 '''
 
 import re
@@ -14,6 +13,8 @@ class GoogleStyler(object):
     __functions = []
     __result = ''
     __report = None
+    __content = ''
+    __code = ''
 
     def __init__(self, styler_selections=[], report=None):
         # currently, all sylers selected and no parameter needed in this func
@@ -34,14 +35,15 @@ class GoogleStyler(object):
                 self.__functions.append(f)
 
     def run_stylers(self, content):
+        self.__content = content
+        self.__code = self.__content.split('\n')
         for f in self.functions():
-            getattr(self, f)(content)
+            getattr(self, f)()
 
-    def styler_single_line_max_characters(self, content=''):
-        code = content.split('\n')
+    def styler_single_line_max_characters(self):
         max_characters = 80
         line_number = 1
-        for string in code:
+        for string in self.__code:
             l = len(string)
             if l > max_characters:
                 output = 'GCS Error exceed max characters ' + str(max_characters) + \
@@ -50,4 +52,13 @@ class GoogleStyler(object):
             line_number += 1
         self.__report.write_to_file(self.__result)
         self.__result = ''
+
+    def styler_exception_format(self):
+        # 1. exception with no description string
+        # 2. exception with 2 argutments
+        # 3. raise a string based exception "raise 'too long !!!'"
+        # 4. never catch all kinds of exceptions
+        # 5. use finally clause to do something after the exception
+        # 6. when catching an exception, use as rather than a comma
+        pass
 
